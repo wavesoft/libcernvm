8a8efeea40c983de30176b0180063b76a180e533
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2014-10-25 19:53:36 +0100

    Fixing linux typo

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -2009,7 +2009,7 @@ unsigned long long getFileTimeMs ( const std::string& file ) {
 
     // Stat file
     struct stat attrib;
-    memset( &attrib, sizeof(struct _stat), 0 );
+    memset( &attrib, sizeof(struct stat), 0 );
     stat( file.c_str(), &attrib);
 
     // Calculate and return milliseconds
